0952d0578b52be0fde070778334770715600eece
Jacob Hoskins <jhoskins997@gmail.com>
2022-03-02 22:55:34 -0800

    Income vs Bills page, dialog box

--- app/src/main/java/com/example/finatrack/IncomeVsBillsPage.java
+++ app/src/main/java/com/example/finatrack/IncomeVsBillsPage.java
@@ -2,7 +2,10 @@ package com.example.finatrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IncomeVsBillsPage extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class IncomeVsBillsPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_income_vs_bills_page);
+    }
+
+    public void AddBillDialog(View view){
+        Button addBill = findViewById(R.id.AddBillID);
+
+        addBill.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Button Clicked");
+                AddBillPopUp();
+            }
+        });
+    }
+
+    private void AddBillPopUp(){
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.add_bill_dialog);
+        dialog.show();
     }
 }
